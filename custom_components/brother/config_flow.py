54e8ff8b7d28eedd9aa940fb0e1e8b27a869e2d6
Maciej Bieniek <maciej.bieniek@gmail.com>
2019-12-03 11:17:59 +0000

    Add hostname/IP validation

--- custom_components/brother/config_flow.py
+++ custom_components/brother/config_flow.py
@@ -1,9 +1,11 @@
 """Adds config flow for Brother Printer."""
+import ipaddress
 import logging
+import re
 
 import voluptuous as vol
 from brother import Brother, SnmpError, UnsupportedModel
-from homeassistant import config_entries
+from homeassistant import config_entries, exceptions
 from homeassistant.const import CONF_HOST
 
 from .const import DOMAIN  # pylint:disable=unused-import
@@ -11,6 +13,17 @@ from .const import DOMAIN  # pylint:disable=unused-import
 _LOGGER = logging.getLogger(__name__)
 
 DATA_SCHEMA = vol.Schema({vol.Required(CONF_HOST, default=""): str})
+
+
+def host_valid(host):
+    try:
+        if ipaddress.ip_address(host).version == (4 or 6):
+            return True
+    except ValueError:
+        disallowed = re.compile(r"[^a-zA-Z\d\-]")
+        if all(map(lambda x: len(x) and not disallowed.search(x), host.split("."))):
+            return True
+        return False
 
 
 class BrotherConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -24,19 +37,17 @@ class BrotherConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is not None:
-            brother = Brother(user_input[CONF_HOST])
-
-            # dodać sprawdzanie regexem nazwy hosta/IP - czy nie ma : albo /
-            # jeśli nie pasuje to raise WrongHost
-
             try:
+                if not host_valid(user_input[CONF_HOST]):
+                    raise InvalidHost()
+                brother = Brother(user_input[CONF_HOST])
                 await brother.async_update()
 
                 return self.async_create_entry(title=brother.model, data=user_input)
-            # except WrongHost:
-            #     errors["base"] = "wrong_address"
-            except SnmpError:
+            except InvalidHost:
                 errors["base"] = "wrong_address"
+            except SnmpError:
+                errors["base"] = "snmp_error"
             except UnsupportedModel:
                 errors["base"] = "unsupported_model"
             except Exception:  # pylint: disable=broad-except
@@ -46,3 +57,7 @@ class BrotherConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
+
+
+class InvalidHost(exceptions.HomeAssistantError):
+    """Error to indicate that hostname/IP address is invalid."""
