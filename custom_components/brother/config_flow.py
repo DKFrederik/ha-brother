67ae28e6859514e7985e6385bfd2fe53deba575b
bieniu <maciej.bieniek@gmail.com>
2019-12-11 09:56:09 +0100

    Change way to create entry

--- custom_components/brother/config_flow.py
+++ custom_components/brother/config_flow.py
@@ -86,10 +86,14 @@ class BrotherConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     if sensor in brother.data:
                         sensors.append(sensor)
 
-                user_input[CONF_SERIAL] = brother.serial.lower()
-                user_input[CONF_SENSORS] = sensors
+                device_data = {
+                    CONF_SERIAL: brother.serial.lower(),
+                    CONF_SENSORS: sensors,
+                }
                 title = f"{brother.model} {brother.serial}"
-                return self.async_create_entry(title=title, data=user_input)
+                return self.async_create_entry(
+                    title=title, data={**user_input, **device_data}
+                )
             except InvalidHost:
                 errors[CONF_HOST] = "wrong_host"
             except ConnectionError:
